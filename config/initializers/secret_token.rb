27ee1e441ae40efcd8a111a62577e352015aba4f
vinahalim <elvina.halim@gmail.com>
2014-10-07 19:33:27 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vinaceous::Application.config.secret_key_base = 'bc855bdc7989e168190924312d8338db72f8069e16578add399e79b257c443c6d7877880aadd1cd49af947aaaf863a8d9d016de02802b8378d72cac953439ceb'
